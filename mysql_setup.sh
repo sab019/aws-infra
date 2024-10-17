f58df844ca8998ef39484a56110d9b4993f381b5
sabri <sboudjennah@free.fr>
2024-10-17 17:21:30 +0200

    fdp

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -4,7 +4,7 @@
 db_backup_private_ip="${db_backup_private_ip}"
 mysql_user="root"  # Changez cela si nécessaire
 mysql_password="root"  # Mettez ici le mot de passe MySQL
-mysql_database="mysql"  # Nom de la base de données à dumper
+mysql_database="test_db"  # Nom de la base de données à dumper
 
 # Rediriger les sorties vers un fichier log
 exec > /var/log/mysql-install.log 2>&1
@@ -24,7 +24,31 @@ echo "Starting MySQL service..."
 systemctl start mysql
 systemctl enable mysql
 
+mysql -u root <<EOF
+-- Création de la base de données
+CREATE DATABASE test_db;
+
+-- Sélection de la base de données
+USE test_db;
+
+-- Création de la table de test
+CREATE TABLE test_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- Colonne pour l'ID unique
+    name VARCHAR(100) NOT NULL,         -- Colonne pour le nom
+    email VARCHAR(100) NOT NULL,        -- Colonne pour l'email
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Colonne pour la date de création
+);
+
+-- Insertion de quelques données de test
+INSERT INTO test_table (name, email)
+VALUES
+    ('Alice Dupont', 'alice.dupont@example.com'),
+    ('Bob Martin', 'bob.martin@example.com'),
+    ('Charlie Legrand', 'charlie.legrand@example.com');
+EOF
+
 # Sécurisation de l'installation MySQL via des commandes SQL
+
 echo "Securing MySQL installation..."
 mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY '$mysql_password';
@@ -47,3 +71,48 @@ echo "Configuring cron jobs for MySQL dump and backup transfer..."
 
 echo "Cron jobs added successfully."
 
+
+echo "Starting user_data script for Web Server"
+
+
+# installe Apache
+apt install apache2 -y
+apt install php7.2 -y
+apt install php-mysql -y
+
+systemctl start apache2
+systemctl enable apache2
+
+cat <<EOL > /var/www/index.php
+<h1>This is Web Server intranet</h1>
+<?php
+// Configuration de la base de données
+$host = 'localhost';
+$dbname = 'test_db';  // Nom de la base de données
+$username = 'root';   // Nom d'utilisateur MySQL
+$password = 'root';       // Mot de passe MySQL
+
+try {
+    // Connexion à la base de données avec PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT * FROM test_table;';
+    $stmt = $pdo->query($sql);
+
+    // Vérification s'il y a des résultats
+        echo '<ul>';
+        // Boucle à travers chaque utilisateur et affichage dans une liste HTML
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo '<li>' . htmlspecialchars($row['name']) . '</li>';
+        }
+        echo '</ul>';
+
+} catch (PDOException $e) {
+    // Gestion des erreurs de connexion ou de requête
+    echo 'Erreur de connexion : ' . $e->getMessage();
+}
+?>
+EOL
+
+echo "User_data script for Web Server completed"
--- terraform.tf
+++ terraform.tf
@@ -405,6 +405,22 @@ resource "aws_security_group" "db" {
     description = "SSH access from within the VPC"
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.76.0.0/16"]
+    description = "acces intranet web"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.76.0.0/16"]
+    description = "acces intranet web https"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
